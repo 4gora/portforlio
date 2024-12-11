ceece6ca9c2d2d51d89849a8f5f0fed08b2ff63e
Alessandro Rosario <121242080+4gora@users.noreply.github.com>
2024-12-10 23:07:35 -0300

    index atualizado, adicionado novos icones

---
+++ index.js
@@ -0,0 +1,13 @@
+let lastScrollTop = 0
+const header = document.querySelector("header")
+
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop
+    if (scrollTop > lastScrollTop) {
+        header.classList.add("hidden")
+    } else {
+        header.classList.remove("hidden")
+    }
+
+    lastScrollTop = scrollTop
+})
