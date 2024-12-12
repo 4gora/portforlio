7667081911dd430b505459b889ca0de36f5bb556
Alessandro Rosario <121242080+4gora@users.noreply.github.com>
2024-12-11 23:34:06 -0300

    primeira versao terminada

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let lastScrollTop = 0
-const header = document.querySelector("header")
+/* Código JS que faz com que a barra de navegação desapareça enquanto rolamos para baixo e reapareça ao rolar para cima*/
+
+let lastScrollTop = 0; // variável que armazena a última posição do scroll
+const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop
-    if (scrollTop > lastScrollTop) {
-        header.classList.add("hidden")
-    } else {
-        header.classList.remove("hidden")
-    }
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+/**
+ * Adiciona ou remove a classe hidden do header com base na direção do scroll
+ * Se o site rolar para baixo, a classe hidden é adicionada
+ * Se o site rolar para cima, a classe hidden é removida
+ */
 
-    lastScrollTop = scrollTop
-})
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+  lastScrollTop = scrollTop;
+});
